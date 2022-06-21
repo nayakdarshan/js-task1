86cc1ca41c15679eba2219f2f67c4ba1e4a9a479
Darshan Nayak <58128151+nayakdarshan22@users.noreply.github.com>
2022-06-21 15:42:05 +0530

    Add files via upload

--- fetchData.js
+++ fetchData.js
@@ -16,7 +16,7 @@ var studentDetails = (function() {
 
 //iterating throughout the studentDetails and appending to the table
 $(document).ready(function() {
-    var table = $('#myTable');
+    var table = $('#studentTable');
 
     var max_size = studentDetails.length;
     var min = 0;
@@ -32,9 +32,9 @@ $(document).ready(function() {
         // console.log(max_size);
         for (var i = min; i < limit; i++) {
             var tab = '<tr><td>' + studentDetails[i].id + "\n" + '</td><td>' + studentDetails[i].name + "\n" + '</td><td>' +
-                studentDetails[i].class + "\n" + '</td><td>' + studentDetails[i].rank;
+                studentDetails[i].class + "\n" + '</td><td class="studentRank">' + studentDetails[i].rank + '</td>';
 
-            $('#myTable').append(tab)
+            $('#studentTable').append(tab)
             slno++;
         }
     }
@@ -76,3 +76,112 @@ $(document).ready(function() {
         appendStudentData(start * 5, limit);
     });
 });
+//sorting by rank function
+function sortByRank() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("studentTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 0; i < (rows.length); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByClassName("studentRank")[0];
+            y = rows[i + 1].getElementsByClassName("studentRank")[0];
+            //check if the two rows should switch place:
+            if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+
+}
+
+//sorting by A to Z function
+function sortByAtoZ() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("studentTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 0; i < (rows.length); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("td")[0];
+            y = rows[i + 1].getElementsByTagName("td")[0];
+            //check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+//sorting by Z to A function
+function sortByZtoA() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("studentTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 0; i < (rows.length); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("td")[0];
+            y = rows[i + 1].getElementsByTagName("td")[0];
+            //check if the two rows should switch place:
+            if (y.innerHTML.toLowerCase() > x.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+
+}
